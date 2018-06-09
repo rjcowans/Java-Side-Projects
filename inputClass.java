83691c6827249158869cc04f63228d95a26519a6
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-09 15:30:55 -0400

    First class application uses. inputClass

---
+++ inputClass.java
@@ -0,0 +1,9 @@
+// nameOfFirstThingApplicationUsesHereSuchAsInputClass
+public class  inputClass {
+   public inputClass(String value) {
+     tellInformation(value);
+   }
+   public void tellInformation(String value) {
+     System.out.println(value);
+   }
+}
