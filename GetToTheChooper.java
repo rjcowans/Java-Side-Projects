ef59597bb5d3e9410d2a5c545561bd33604c7118
rjcowans <rcowans11@gmail.com>
2018-06-09 11:03:32 -0400

    This is the same way but in one file

---
+++ GetToTheChooper.java
@@ -0,0 +1,25 @@
+public class  GetToTheChooper{
+   public static void main(String args[]){
+      runner app = new runner(3);
+   }
+}
+
+class runner{
+   public runner(int test){
+      // this is your new main.
+      
+      function1(test);
+      System.out.println(function2(test));
+      System.out.println(function3(test));
+      Chopper chopper = new Chopper();
+   }
+   
+   public void function1(int test){System.out.println(test);}
+   public int function2(int test){ return test;}
+   public String function3(int test){return "test";}
+}
+ class  Chopper{
+   public Chopper(){
+   
+   }
+}
